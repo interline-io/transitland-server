f1e879362c44e192ac51b94631d8fc430e41b1c9
Ian Rees <ian@ianrees.net>
2021-07-28 22:22:01 -0700

    Use server dmfr

--- test_setup.sh
+++ test_setup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-transitland dmfr sync -dburl=$1 test/data/server/server-test.dmfr.json
-transitland dmfr fetch -dburl=$1 -gtfsdir=$TL_TEST_GTFSDIR
-transitland dmfr import -dburl=$1 -gtfsdir=$TL_TEST_GTFSDIR -activate
-transitland dmfr sync -dburl=$1 test/data/server/server-test.dmfr.json
+transitland_server dmfr sync -dburl=$1 test/data/server/server-test.dmfr.json
+transitland_server dmfr fetch -dburl=$1 -gtfsdir=$TL_TEST_GTFSDIR
+transitland_server dmfr import -dburl=$1 -gtfsdir=$TL_TEST_GTFSDIR -activate
+transitland_server dmfr sync -dburl=$1 test/data/server/server-test.dmfr.json
