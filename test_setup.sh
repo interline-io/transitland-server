8f5ba49de1b73d0e7d3896feac7f37bb462fe738
Ian Rees <ian@ianrees.net>
2021-08-11 23:16:39 -0700

    Update setup script

--- test_setup.sh
+++ test_setup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-tlserver dmfr sync -dburl=$1 test/data/server/server-test.dmfr.json
-tlserver dmfr fetch -dburl=$1 -gtfsdir=$TL_TEST_GTFSDIR
-tlserver dmfr import -dburl=$1 -gtfsdir=$TL_TEST_GTFSDIR -activate
-tlserver dmfr sync -dburl=$1 test/data/server/server-test.dmfr.json
+tlserver sync -dburl=$1 test/data/server/server-test.dmfr.json
+tlserver fetch -dburl=$1 -gtfsdir=$TL_TEST_GTFSDIR
+tlserver import -dburl=$1 -gtfsdir=$TL_TEST_GTFSDIR -activate
+tlserver sync -dburl=$1 test/data/server/server-test.dmfr.json
