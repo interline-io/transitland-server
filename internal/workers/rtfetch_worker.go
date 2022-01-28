fa8f9a4778afe141deebaa0725e72288c1ce2457
Ian Rees <ian@ianrees.net>
2022-01-27 23:27:55 -0800

    Improve rt feed fetching logic (#30)

--- internal/workers/rtfetch_worker.go
+++ internal/workers/rtfetch_worker.go
@@ -14,20 +14,23 @@ type RTFetchWorker struct{}
 
 func (w *RTFetchWorker) Run(ctx context.Context, job jobs.Job) error {
 	// fmt.Printf("fetch worker! job: %#v\n", job)
-	if len(job.Args) != 3 {
+	if len(job.Args) != 4 {
 		return errors.New("feed, type and url required")
 	}
 	feed := job.Args[0]
 	ftype := job.Args[1]
 	url := job.Args[2]
+	source := job.Args[3]
 	var rtdata []byte
 	msg, err := rt.ReadURL(url)
 	if err != nil {
-		return err
+		fmt.Printf("source '%s' type '%s': request failed\n", source, ftype)
+		return nil
 	}
 	rtdata, err = proto.Marshal(msg)
 	if err != nil {
-		return err
+		fmt.Printf("source '%s' type '%s': failed to parse response\n", source, ftype)
+		return nil
 	}
 	key := fmt.Sprintf("rtdata:%s:%s", feed, ftype)
 	return job.Opts.RTFinder.AddData(key, rtdata)
