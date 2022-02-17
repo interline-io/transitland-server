dae376f82641144409701b13eb9f2ce8441290cb
Ian Rees <ian@ianrees.net>
2022-02-16 19:34:35 -0800

    Tidy

--- test_setup.sh
+++ test_setup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-export TL_LOG=debug
+# export TL_LOG=debug
 tlserver sync test/data/server/server-test.dmfr.json
 tlserver fetch -gtfsdir=$TL_TEST_GTFSDIR
 tlserver import -gtfsdir=$TL_TEST_GTFSDIR -activate
