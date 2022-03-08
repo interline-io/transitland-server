0b98daebc5509cd4e07ed7c14a4cd193ea6c0e76
Ian Rees <ian@ianrees.net>
2022-03-08 13:00:47 -0800

    Make test_setup.sh dburl explicit

--- test_setup.sh
+++ test_setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # export TL_LOG=debug
-tlserver sync test/data/server/server-test.dmfr.json
-tlserver fetch -gtfsdir=$TL_TEST_GTFSDIR
-tlserver import -gtfsdir=$TL_TEST_GTFSDIR -activate
-tlserver sync test/data/server/server-test.dmfr.json
+tlserver sync -dburl="$TL_TEST_SERVER_DATABASE_URL" test/data/server/server-test.dmfr.json
+tlserver fetch -dburl="$TL_TEST_SERVER_DATABASE_URL"  -gtfsdir="$TL_TEST_GTFSDIR"
+tlserver import -dburl="$TL_TEST_SERVER_DATABASE_URL" -gtfsdir="$TL_TEST_GTFSDIR" -activate
+tlserver sync -dburl="$TL_TEST_SERVER_DATABASE_URL" test/data/server/server-test.dmfr.json
