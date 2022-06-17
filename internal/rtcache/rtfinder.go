63ccf80282620ff008e8c2b48c54ed99af3f1fc7
Ian Rees <ian@ianrees.net>
2022-06-16 18:10:37 -0700

    Various improvements to rt caching (#83)

--- internal/rtcache/rtfinder.go
+++ internal/rtcache/rtfinder.go
@@ -2,8 +2,8 @@ package rtcache
 
 import (
 	"errors"
+	"fmt"
 	"strconv"
-	"sync"
 	"time"
 
 	"github.com/interline-io/transitland-lib/log"
@@ -12,18 +12,25 @@ import (
 	"github.com/jmoiron/sqlx"
 )
 
+// Cache provides a method for looking up and listening for changed RT data
+type Cache interface {
+	AddFeedMessage(string, *pb.FeedMessage) error
+	AddData(string, []byte) error
+	GetSource(string) (*Source, bool)
+	Close() error
+}
+
+////////
+
 type RTFinder struct {
-	cache    Cache
-	fetchers map[string]*rtConsumer
-	lock     sync.Mutex
-	lc       *lookupCache
+	cache Cache
+	lc    *lookupCache
 }
 
 func NewRTFinder(cache Cache, db sqlx.Ext) *RTFinder {
 	return &RTFinder{
-		cache:    cache,
-		lc:       newLookupCache(db),
-		fetchers: map[string]*rtConsumer{},
+		cache: cache,
+		lc:    newLookupCache(db),
 	}
 }
 
@@ -40,25 +47,31 @@ func (f *RTFinder) StopTimezone(id int, known string) (*time.Location, bool) {
 }
 
 func (f *RTFinder) FindTrip(t *model.Trip) *pb.TripUpdate {
-	topic, _ := f.lc.GetFeedVersionOnestopID(t.FeedVersionID)
-	a, _ := f.getTrip(topic, t.TripID)
-	return a
+	topics, _ := f.lc.GetFeedVersionRTFeeds(t.FeedVersionID)
+	for _, topic := range topics {
+		if a, ok := f.getTrip(topic, t.TripID); ok {
+			return a
+		}
+	}
+	return nil
 }
 
 func (f *RTFinder) FindAlertsForTrip(t *model.Trip) []*model.Alert {
-	topic, _ := f.lc.GetFeedVersionOnestopID(t.FeedVersionID)
-	a, err := f.getListener(getTopicKey(topic, "realtime_alerts"))
-	if err != nil {
-		return nil
-	}
 	var foundAlerts []*model.Alert
-	for _, alert := range a.alerts {
-		for _, s := range alert.GetInformedEntity() {
-			if s.Trip == nil {
-				continue
-			}
-			if s.Trip.GetTripId() == t.TripID {
-				foundAlerts = append(foundAlerts, makeAlert(alert))
+	topics, _ := f.lc.GetFeedVersionRTFeeds(t.FeedVersionID)
+	for _, topic := range topics {
+		a, ok := f.cache.GetSource(getTopicKey(topic, "realtime_alerts"))
+		if !ok {
+			return nil
+		}
+		for _, alert := range a.alerts {
+			for _, s := range alert.GetInformedEntity() {
+				if s.Trip == nil {
+					continue
+				}
+				if s.Trip.GetTripId() == t.TripID {
+					foundAlerts = append(foundAlerts, makeAlert(alert))
+				}
 			}
 		}
 	}
@@ -66,19 +79,21 @@ func (f *RTFinder) FindAlertsForTrip(t *model.Trip) []*model.Alert {
 }
 
 func (f *RTFinder) FindAlertsForRoute(t *model.Route) []*model.Alert {
-	topic, _ := f.lc.GetFeedVersionOnestopID(t.FeedVersionID)
-	a, err := f.getListener(getTopicKey(topic, "realtime_alerts"))
-	if err != nil {
-		return nil
-	}
 	var foundAlerts []*model.Alert
-	for _, alert := range a.alerts {
-		for _, s := range alert.GetInformedEntity() {
-			if s.Trip != nil {
-				continue
-			}
-			if s.GetRouteId() == t.RouteID {
-				foundAlerts = append(foundAlerts, makeAlert(alert))
+	topics, _ := f.lc.GetFeedVersionRTFeeds(t.FeedVersionID)
+	for _, topic := range topics {
+		a, ok := f.cache.GetSource(getTopicKey(topic, "realtime_alerts"))
+		if !ok {
+			continue
+		}
+		for _, alert := range a.alerts {
+			for _, s := range alert.GetInformedEntity() {
+				if s.Trip != nil {
+					continue
+				}
+				if s.GetRouteId() == t.RouteID {
+					foundAlerts = append(foundAlerts, makeAlert(alert))
+				}
 			}
 		}
 	}
@@ -86,19 +101,21 @@ func (f *RTFinder) FindAlertsForRoute(t *model.Route) []*model.Alert {
 }
 
 func (f *RTFinder) FindAlertsForAgency(t *model.Agency) []*model.Alert {
-	topic, _ := f.lc.GetFeedVersionOnestopID(t.FeedVersionID)
-	a, err := f.getListener(getTopicKey(topic, "realtime_alerts"))
-	if err != nil {
-		return nil
-	}
 	var foundAlerts []*model.Alert
-	for _, alert := range a.alerts {
-		for _, s := range alert.GetInformedEntity() {
-			if s.Trip != nil {
-				continue
-			}
-			if s.GetAgencyId() == t.AgencyID {
-				foundAlerts = append(foundAlerts, makeAlert(alert))
+	topics, _ := f.lc.GetFeedVersionRTFeeds(t.FeedVersionID)
+	for _, topic := range topics {
+		a, ok := f.cache.GetSource(getTopicKey(topic, "realtime_alerts"))
+		if !ok {
+			continue
+		}
+		for _, alert := range a.alerts {
+			for _, s := range alert.GetInformedEntity() {
+				if s.Trip != nil {
+					continue
+				}
+				if s.GetAgencyId() == t.AgencyID {
+					foundAlerts = append(foundAlerts, makeAlert(alert))
+				}
 			}
 		}
 	}
@@ -106,19 +123,21 @@ func (f *RTFinder) FindAlertsForAgency(t *model.Agency) []*model.Alert {
 }
 
 func (f *RTFinder) FindAlertsForStop(t *model.Stop) []*model.Alert {
-	topic, _ := f.lc.GetFeedVersionOnestopID(t.FeedVersionID)
-	a, err := f.getListener(getTopicKey(topic, "realtime_alerts"))
-	if err != nil {
-		return nil
-	}
 	var foundAlerts []*model.Alert
-	for _, alert := range a.alerts {
-		for _, s := range alert.GetInformedEntity() {
-			if s.StopId == nil {
-				continue
-			}
-			if s.GetStopId() == t.StopID {
-				foundAlerts = append(foundAlerts, makeAlert(alert))
+	topics, _ := f.lc.GetFeedVersionRTFeeds(t.FeedVersionID)
+	for _, topic := range topics {
+		a, ok := f.cache.GetSource(getTopicKey(topic, "realtime_alerts"))
+		if !ok {
+			continue
+		}
+		for _, alert := range a.alerts {
+			for _, s := range alert.GetInformedEntity() {
+				if s.StopId == nil {
+					continue
+				}
+				if s.GetStopId() == t.StopID {
+					foundAlerts = append(foundAlerts, makeAlert(alert))
+				}
 			}
 		}
 	}
@@ -126,58 +145,62 @@ func (f *RTFinder) FindAlertsForStop(t *model.Stop) []*model.Alert {
 }
 
 func (f *RTFinder) FindStopTimeUpdate(t *model.Trip, st *model.StopTime) (*pb.TripUpdate_StopTimeUpdate, bool) {
-	topic, _ := f.lc.GetFeedVersionOnestopID(t.FeedVersionID)
 	tid := t.TripID
 	seq := st.StopSequence
-	rtTrip, rtok := f.getTrip(topic, tid)
-	if !rtok {
-		return nil, false
-	}
-	// Match on stop sequence
-	for _, ste := range rtTrip.StopTimeUpdate {
-		if int(ste.GetStopSequence()) == seq {
-			log.Trace().Str("trip_id", t.TripID).Int("seq", seq).Msgf("found stop time update on trip_id/stop_sequence")
-			return ste, true
+	topics, _ := f.lc.GetFeedVersionRTFeeds(t.FeedVersionID)
+	for _, topic := range topics {
+		rtTrip, rtok := f.getTrip(topic, tid)
+		if !rtok {
+			continue
+		}
+		// Match on stop sequence
+		for _, ste := range rtTrip.StopTimeUpdate {
+			if int(ste.GetStopSequence()) == seq {
+				log.Trace().Str("trip_id", t.TripID).Int("seq", seq).Msgf("found stop time update on trip_id/stop_sequence")
+				return ste, true
+			}
+		}
+		// If no match on stop sequence, match on stop_id if stop is not visited twice
+		check := map[string]int{}
+		for _, ste := range rtTrip.StopTimeUpdate {
+			check[ste.GetStopId()] += 1
+		}
+		sid, ok := f.lc.GetGtfsStopID(atoi(st.StopID))
+		if !ok {
+			continue
+		}
+		for _, ste := range rtTrip.StopTimeUpdate {
+			stid := ste.GetStopId()
+			if sid == stid && check[stid] == 1 {
+				log.Trace().Str("trip_id", t.TripID).Str("stop_id", sid).Msgf("found stop time update on trip_id/stop_id")
+				return ste, true
+			}
 		}
 	}
-	// If no match on stop sequence, match on stop_id if stop is not visited twice
-	check := map[string]int{}
-	for _, ste := range rtTrip.StopTimeUpdate {
-		check[ste.GetStopId()] += 1
-	}
-	sid, ok := f.lc.GetGtfsStopID(atoi(st.StopID))
-	if !ok {
-		return nil, false
-	}
-	for _, ste := range rtTrip.StopTimeUpdate {
-		stid := ste.GetStopId()
-		if sid == stid && check[stid] == 1 {
-			log.Trace().Str("trip_id", t.TripID).Str("stop_id", sid).Msgf("found stop time update on trip_id/stop_id")
-			return ste, true
-		}
-	}
-	log.Trace().Str("trip_id", t.TripID).Int("seq", seq).Str("stop_id", sid).Msgf("no stop time update found")
+	log.Trace().Str("trip_id", t.TripID).Int("seq", seq).Msgf("no stop time update found")
 	return nil, false
 }
 
 // TODO: put this method on consumer and wrap, as with GetTrip
 func (f *RTFinder) GetAddedTripsForStop(t *model.Stop) []*pb.TripUpdate {
-	topic, _ := f.lc.GetFeedVersionOnestopID(t.FeedVersionID)
 	sid := t.StopID
-	a, err := f.getListener(getTopicKey(topic, "realtime_trip_updates"))
-	if err != nil {
-		return nil
-	}
-	// TODO: index more efficiently
 	var ret []*pb.TripUpdate
-	for _, trip := range a.entityByTrip {
-		if trip.Trip.GetScheduleRelationship() != pb.TripDescriptor_ADDED {
+	topics, _ := f.lc.GetFeedVersionRTFeeds(t.FeedVersionID)
+	for _, topic := range topics {
+		a, ok := f.cache.GetSource(getTopicKey(topic, "realtime_trip_updates"))
+		if !ok {
 			continue
 		}
-		for _, ste := range trip.StopTimeUpdate {
-			if ste.GetStopId() == sid {
-				ret = append(ret, trip)
-				break // continue to next trip
+		// TODO: index more efficiently
+		for _, trip := range a.entityByTrip {
+			if trip.Trip.GetScheduleRelationship() != pb.TripDescriptor_ADDED {
+				continue
+			}
+			for _, ste := range trip.StopTimeUpdate {
+				if ste.GetStopId() == sid {
+					ret = append(ret, trip)
+					break // continue to next trip
+				}
 			}
 		}
 	}
@@ -206,33 +229,12 @@ func (f *RTFinder) getTrip(topic string, tid string) (*pb.TripUpdate, bool) {
 	if tid == "" {
 		return nil, false
 	}
-	a, err := f.getListener(getTopicKey(topic, "realtime_trip_updates"))
-	if err != nil {
+	a, ok := f.cache.GetSource(getTopicKey(topic, "realtime_trip_updates"))
+	if !ok {
 		return nil, false
 	}
 	trip, ok := a.GetTrip(tid)
 	return trip, ok
-}
-
-func (f *RTFinder) getListener(topicKey string) (*rtConsumer, error) {
-	f.lock.Lock()
-	a, ok := f.fetchers[topicKey]
-	if !ok {
-		ch, err := f.cache.Listen(topicKey)
-		// Failed to create listener
-		if err != nil {
-			log.Error().Err(err).Str("topic", topicKey).Msg("rtfinder: failed to create listener")
-			return nil, err
-		}
-		log.Trace().Err(err).Str("topic", topicKey).Msg("rtfinder: listener created")
-		a, _ = newRTConsumer()
-		a.feed = topicKey
-		a.Start(ch)
-		log.Trace().Err(err).Str("topic", topicKey).Msg("rtfinder: started consumer")
-		f.fetchers[topicKey] = a
-	}
-	f.lock.Unlock()
-	return a, nil
 }
 
 func makeAlert(a *pb.Alert) *model.Alert {
@@ -288,4 +290,8 @@ func newTranslation(v *pb.TranslatedString) []*model.RTTranslation {
 		ret = append(ret, &ntr)
 	}
 	return ret
+}
+
+func getTopicKey(topic string, t string) string {
+	return fmt.Sprintf("rtdata:%s:%s", topic, t)
 }
