76754bca5f9569cadcbf0b91779d42952f15c705
Ian Rees <ian@ianrees.net>
2022-06-16 17:28:09 -0700

    Revert "Revert "Various improvements to rt caching""
    This reverts commit 73312ab969f118d4153284aec2917ad91e7a829d.

---
+++ internal/rtcache/local.go
@@ -0,0 +1,45 @@
+package rtcache
+
+import (
+	"sync"
+
+	"github.com/interline-io/transitland-lib/rt/pb"
+)
+
+type LocalCache struct {
+	lock    sync.Mutex
+	sources map[string]*Source
+}
+
+func NewLocalCache() *LocalCache {
+	return &LocalCache{
+		sources: map[string]*Source{},
+	}
+}
+
+func (f *LocalCache) GetSource(topic string) (*Source, bool) {
+	a, ok := f.sources[topic]
+	if ok {
+		return a, true
+	}
+	return nil, false
+}
+
+func (f *LocalCache) AddFeedMessage(topic string, rtmsg *pb.FeedMessage) error {
+	return nil
+}
+
+func (f *LocalCache) AddData(topic string, data []byte) error {
+	f.lock.Lock()
+	defer f.lock.Unlock()
+	s, ok := f.sources[topic]
+	if !ok {
+		s, _ = NewSource(topic)
+		f.sources[topic] = s
+	}
+	return s.process(data)
+}
+
+func (f *LocalCache) Close() error {
+	return nil
+}
