c2fa6d37ff50d5c541f59aedad534b9ff4a28dd4
Drew Dara-Abrams <drew@interline.io>
2022-08-02 13:40:31 -0700

    use `RestPrefix` when defined

--- rest/onestop_id_entity_request.go
+++ rest/onestop_id_entity_request.go
@@ -13,14 +13,14 @@ func onestopIdEntityRedirectHandler(cfg restConfig, w http.ResponseWriter, r *ht
 	onestop_id := mux.Vars(r)["onestop_id"]
 	var redirectUrl string
 	if strings.HasPrefix(onestop_id, "f-") {
-		redirectUrl = fmt.Sprintf("/rest/feeds/%s", onestop_id)
+		redirectUrl = fmt.Sprintf("%s/feeds/%s", cfg.RestPrefix, onestop_id)
 		// redirect to feeds/
 	} else if strings.HasPrefix(onestop_id, "o-") {
-		redirectUrl = fmt.Sprintf("/rest/operators/%s", onestop_id)
+		redirectUrl = fmt.Sprintf("%s/operators/%s", cfg.RestPrefix, onestop_id)
 	} else if strings.HasPrefix(onestop_id, "s-") {
-		redirectUrl = fmt.Sprintf("/rest/stops/%s", onestop_id)
+		redirectUrl = fmt.Sprintf("%s/stops/%s", cfg.RestPrefix, onestop_id)
 	} else if strings.HasPrefix(onestop_id, "r-") {
-		redirectUrl = fmt.Sprintf("/rest/routes/%s", onestop_id)
+		redirectUrl = fmt.Sprintf("%s/routes/%s", cfg.RestPrefix, onestop_id)
 	}
 	if redirectUrl != "" {
 		w.Header().Add("Location", redirectUrl)
