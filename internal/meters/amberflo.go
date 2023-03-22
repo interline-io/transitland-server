72da702c7d7645269e78c37fe02df516a68f6f36
Ian Rees <ian@ianrees.net>
2023-03-21 17:54:41 -0700

    AmberFlo metering

---
+++ internal/meters/amberflo.go
@@ -0,0 +1,103 @@
+package meters
+
+import (
+	"encoding/json"
+	"io/ioutil"
+	"time"
+
+	"github.com/amberflo/metering-go/v2"
+	"github.com/xtgo/uuid"
+)
+
+type AmberFlo struct {
+	client   *metering.Metering
+	meterMap map[string]amberFloConfig
+}
+
+func NewAmberFlo(apikey string) *AmberFlo {
+	intervalSeconds := 30 * time.Second
+	batchSize := 5
+	debug := true
+	meteringClient := metering.NewMeteringClient(
+		apikey,
+		metering.WithBatchSize(batchSize),
+		metering.WithIntervalSeconds(intervalSeconds),
+		metering.WithDebug(debug),
+	)
+	return &AmberFlo{
+		client:   meteringClient,
+		meterMap: map[string]amberFloConfig{},
+	}
+}
+
+type amberFloConfig struct {
+	Name        string            `json:"name,omitempty"`
+	DefaultUser string            `json:"default_user,omitempty"`
+	Dimensions  map[string]string `json:"dimensions,omitempty"`
+}
+
+func (m *AmberFlo) LoadMeterMap(path string) error {
+	meterMap := map[string]amberFloConfig{}
+	data, err := ioutil.ReadFile(path)
+	if err != nil {
+		return err
+	}
+	if err := json.Unmarshal(data, &meterMap); err != nil {
+		return err
+	}
+	m.meterMap = meterMap
+	return nil
+}
+
+func (m *AmberFlo) NewMeter(handlerName string) ApiMeter {
+	cfg := amberFloConfig{
+		Name: handlerName,
+	}
+	if a, ok := m.meterMap[handlerName]; ok {
+		cfg = amberFloConfig{
+			Name:        a.Name,
+			DefaultUser: a.DefaultUser,
+		}
+		for k, v := range a.Dimensions {
+			cfg.Dimensions[k] = v
+		}
+	}
+	return &amberFloMeter{
+		cfg:    cfg,
+		client: m.client,
+	}
+}
+
+type amberFloMeter struct {
+	cfg    amberFloConfig
+	client *metering.Metering
+}
+
+func (m *amberFloMeter) Meter(e MeterEvent) error {
+	uniqueId := uuid.NewRandom().String()
+	utcMillis := time.Now().UnixNano() / int64(time.Millisecond)
+	userId := m.cfg.DefaultUser
+	if e.UserID != "" {
+		userId = e.UserID
+	}
+	value := e.MeterValue
+	dims := map[string]string{}
+	for k, v := range m.cfg.Dimensions {
+		dims[k] = v
+	}
+	for k, v := range e.Dimensions {
+		dims[k] = v
+	}
+	return m.client.Meter(&metering.MeterMessage{
+		MeterApiName:      m.cfg.Name,
+		UniqueId:          uniqueId,
+		MeterTimeInMillis: utcMillis,
+		CustomerId:        userId,
+		MeterValue:        value,
+		Dimensions:        dims,
+	})
+}
+
+func (m *amberFloMeter) Close() error {
+	return m.client.Shutdown()
+}
