a97d698aa3fb7ff0d25b21dcb70393292db98229
Ian Rees <ian@ianrees.net>
2023-07-13 22:19:00 -0700

    GBFS fetch creates feed_fetch record

--- workers/gbfs_fetch_worker.go
+++ workers/gbfs_fetch_worker.go
@@ -5,21 +5,21 @@ import (
 	"errors"
 	"fmt"
 
+	"github.com/interline-io/transitland-lib/tldb"
 	"github.com/interline-io/transitland-server/internal/gbfs"
 	"github.com/interline-io/transitland-server/internal/jobs"
 	"github.com/interline-io/transitland-server/model"
 )
 
 type GbfsFetchWorker struct {
-	Target       string `json:"target"`
-	Url          string `json:"url"`
-	SourceType   string `json:"source_type"`
-	SourceFeedID string `json:"source_feed_id"`
+	Url        string `json:"url"`
+	FeedID     string `json:"feed_id"`
+	FetchEpoch int64  `json:"fetch_epoch"`
 }
 
 func (w *GbfsFetchWorker) Run(ctx context.Context, job jobs.Job) error {
-	log := job.Opts.Logger.With().Str("target", w.Target).Str("source_feed_id", w.SourceFeedID).Str("source_type", w.SourceType).Str("url", w.Url).Logger()
-	gfeeds, err := job.Opts.Finder.FindFeeds(ctx, nil, nil, nil, nil, &model.FeedFilter{OnestopID: &w.SourceFeedID})
+	log := job.Opts.Logger.With().Str("feed_id", w.FeedID).Str("url", w.Url).Logger()
+	gfeeds, err := job.Opts.Finder.FindFeeds(ctx, nil, nil, nil, nil, &model.FeedFilter{OnestopID: &w.FeedID})
 	if err != nil {
 		log.Error().Err(err).Msg("gbfsfetch worker: error loading source feed")
 		return err
@@ -28,22 +28,29 @@ func (w *GbfsFetchWorker) Run(ctx context.Context, job jobs.Job) error {
 		log.Error().Err(err).Msg("gbfsfetch worker: source feed not found")
 		return errors.New("feed not found")
 	}
+
 	// Make request
 	opts := gbfs.Options{}
 	opts.FeedURL = gfeeds[0].URLs.GbfsAutoDiscovery
+	opts.FeedID = gfeeds[0].ID
+	opts.URLType = "gbfs_auto_discovery"
 	if w.Url != "" {
 		opts.FeedURL = w.Url
 	}
-	feeds, result, err := gbfs.Fetch(opts)
+	feeds, result, err := gbfs.Fetch(
+		tldb.NewPostgresAdapterFromDBX(job.Opts.Finder.DBX()),
+		opts,
+	)
 	if err != nil {
 		return err
 	}
 	if result.FetchError != nil {
 		return result.FetchError
 	}
+
 	// Save to cache
 	for _, feed := range feeds {
-		key := fmt.Sprintf("%s:%s", w.SourceFeedID, feed.SystemInformation.Language.Val)
+		key := fmt.Sprintf("%s:%s", w.FeedID, feed.SystemInformation.Language.Val)
 		job.Opts.GbfsFinder.AddData(ctx, key, feed)
 	}
 	log.Info().Msg("gbfs fetch worker: success")
