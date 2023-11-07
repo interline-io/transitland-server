630593c1cc5f0dcdc46016abc753e2c0fdf4c929
Ian Rees <ian@ianrees.net>
2023-11-06 19:09:49 -0800

    Remove

--- internal/ecache/ecache.go
+++ internal/ecache/ecache.go
@@ -67,12 +67,6 @@ func (e *Cache[T]) Get(ctx context.Context, key string) (T, bool) {
 	return v.Value, ok
 }
 
-func (e *Cache[T]) ClearLocal() {
-	e.lock.Lock()
-	defer e.lock.Unlock()
-	e.m = map[string]Item[T]{}
-}
-
 func (e *Cache[T]) LocalKeys() []string {
 	e.lock.Lock()
 	defer e.lock.Unlock()
