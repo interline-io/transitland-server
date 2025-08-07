7f5ad57d165f66ac6600b8011ebaa8eadf26eb92
Ian Rees <ian@ianrees.net>
2025-08-06 18:25:12 -0700

    Install protoc in CI

--- server/auth/authz/azpb_gen.go
+++ server/auth/authz/azpb_gen.go
@@ -1,4 +1,3 @@
 //go:generate protoc --plugin=protoc-gen-go=../../../protoc-gen-go-wrapper.sh --go_out=. --go-grpc_out=. --go-grpc_opt=paths=source_relative --go-grpc_opt=Mazpb.proto=./authz  --go_opt=paths=source_relative --go_opt=Mazpb.proto=./authz  azpb.proto
-//                      --plugin=protoc-gen-go=../../protoc-gen-go-wrapper.sh --go_out=. --go_opt=paths=source_relative --go_opt=Mgtfs-realtime.proto=rt/pb  gtfs-realtime.proto
 
 package authz
